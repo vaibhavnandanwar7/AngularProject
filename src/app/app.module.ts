31fa16acab0ddc505d9c7a125c4b4d1ba78ba9a3
vaibhavnandanwar7 <vaibhavnandanwar7@gmail.com>
2019-05-12 18:08:37 +0530

    angular practise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { ProductService } from './products/product.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ProductListComponent,
+    ConvertToSpacesPipe, StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService], // for all components
   bootstrap: [AppComponent]
 })
 export class AppModule { }
