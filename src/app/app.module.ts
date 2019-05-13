ffb8a1e53fbdcde0810a5ca82e2831ec327dacf1
vaibhavnandanwar7 <vaibhavnandanwar7@gmail.com>
2019-05-13 23:05:39 +0530

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductService } from './products/product.service';
-
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent, ProductListComponent,
-    ConvertToSpacesPipe, StarComponent
+    ConvertToSpacesPipe, StarComponent, 
+    ProductDetailComponent, WelcomeComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpClientModule
