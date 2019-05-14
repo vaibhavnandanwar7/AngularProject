d8bdc8f2f40fa594ee4d2b4d2a85ed00321e0a61
vaibhavnandanwar7 <vaibhavnandanwar7@gmail.com>
2019-05-14 22:30:20 +0530

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -11,15 +12,23 @@ import { StarComponent } from './shared/star.component';
 import { ProductService } from './products/product.service';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
     AppComponent, ProductListComponent,
-    ConvertToSpacesPipe, StarComponent, 
+    ConvertToSpacesPipe, StarComponent,
     ProductDetailComponent, WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, FormsModule, HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id',canActivate:[ProductDetailGuard], component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ])
   ],
   providers: [ProductService], // for all components
   bootstrap: [AppComponent]
